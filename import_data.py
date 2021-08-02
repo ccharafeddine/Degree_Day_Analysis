73f8583d394d87f4a98ce5d69a439a0b0ffd6bfe
rdillens <remydillenseger@gmail.com>
2021-08-02 17:14:06 -0500

    update date merge code and begin filter by Settlement Point

--- import_data.py
+++ import_data.py
@@ -9,19 +9,19 @@ rtm_path_prefix = 'Resources/ERCOT/rpt.00013061.0000000000000000.RTMLZHBSPP_'
 
 def get_ercot_year_df(path_prefix, year_str):
     csv_path = Path(path_prefix + year_str + '.xlsx')
-    df = pd.read_excel(csv_path, sheet_name=month_list[0])
-    for month in month_list[1:]:
-        temp_df = pd.read_excel(csv_path, sheet_name=month)
-        df = pd.concat([df, temp_df], ignore_index=True)
+    df = pd.read_excel(csv_path, sheet_name=month_list[1])
+#    for month in month_list[1:2]:
+#        temp_df = pd.read_excel(csv_path, sheet_name=month)
+#        df = pd.concat([df, temp_df], ignore_index=True)
     return df
 
 
 def run():
-    print(f'Importing ERCOT {year_list[0]} data from CSV into DataFrame...', end='')
-    ercot_df = get_ercot_year_df(rtm_path_prefix, year_list[0])
+    print(f'Importing ERCOT {year_list[1]} data from CSV into DataFrame...', end='')
+    ercot_df = get_ercot_year_df(rtm_path_prefix, year_list[1])
     print('Done!')
     print(ercot_df.head())
-    for year in year_list[1:]:
+    for year in year_list[2:]:
         print(f'Importing ERCOT {year} data from CSV into DataFrame...', end='')
         new_df = get_ercot_year_df(rtm_path_prefix, year)
         print('Done!')
